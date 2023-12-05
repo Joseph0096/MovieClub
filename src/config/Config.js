cfb28136f280e407a0fbf9ffba738d01fa41fd2c
Joseph0096 <joseph.hagop01@outlook.com>
2023-12-05 19:35:41 +1100

    all commits

---
+++ src/config/Config.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+  apiKey: "AIzaSyDjXvglvBVoMjbufGjec2XFb4pBPTOomJU",
+  authDomain: "movieclub-18fa9.firebaseapp.com",
+  projectId: "movieclub-18fa9",
+  storageBucket: "movieclub-18fa9.appspot.com",
+  messagingSenderId: "632039087075",
+  appId: "1:632039087075:web:bb8f7d91a51606d3f85ed6"
+}
